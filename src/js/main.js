0168d62cf4434bd5212f0748ec5f63c019e45d98
imansaparov <malikimansaparov@gmail.com>
2021-10-16 09:28:33 +0600

    feat: implement modals and sliders moduls

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import modals from './modules/modals';
+import sliders from './modules/sliders';
+
+window.addEventListener('DOMContentLoaded', () => {
+  modals();
+  sliders(
+    '.feedback-slider-item',
+    'horizontal',
+    '.main-prev-btn',
+    '.main-next-btn'
+  );
+  sliders('.main-slider-item', 'horizontal');
+});
