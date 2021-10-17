a5fe98ebf7aac0da535953126576d9507001a448
imansaparov <malikimansaparov@gmail.com>
2021-10-17 08:51:30 +0600

    feat: implement mask and data reguest to server

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
+import checkTextInputs from './modules/checkTextInputs';
+import forms from './modules/forms';
+import mask from './modules/mask';
 import modals from './modules/modals';
+import showMoreStyles from './modules/showMoreStyles';
 import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -10,4 +14,9 @@ window.addEventListener('DOMContentLoaded', () => {
     '.main-next-btn'
   );
   sliders('.main-slider-item', 'horizontal');
+  forms();
+  mask('[name="phone"]');
+  checkTextInputs('[name="name"]');
+  checkTextInputs('[name="message"]');
+  showMoreStyles('.button-styles', '#styles .row');
 });
