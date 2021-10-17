f37fe5c4b8fc56c0017cfbe6d458c2a24b69efde
imansaparov <malikimansaparov@gmail.com>
2021-10-17 11:39:03 +0600

    feat implement filter for picture and calc price

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
+import calc from './modules/calc';
 import checkTextInputs from './modules/checkTextInputs';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import modals from './modules/modals';
 import showMoreStyles from './modules/showMoreStyles';
 import sliders from './modules/sliders';
+import filter from './modules/filter';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
@@ -19,4 +21,6 @@ window.addEventListener('DOMContentLoaded', () => {
   checkTextInputs('[name="name"]');
   checkTextInputs('[name="message"]');
   showMoreStyles('.button-styles', '#styles .row');
+  calc('#size', '#material', '#options', '.promocode', '.calc-price');
+  filter();
 });
