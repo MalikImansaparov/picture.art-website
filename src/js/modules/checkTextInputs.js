da07e1886000ad1c93124e989d48bfc3f0c01a38
Imansaparov <Malikimansaparov@gmail.com>
2021-10-20 07:34:21 +0600

    feat: implement check for enter Russian register

--- src/js/modules/checkTextInputs.js
+++ src/js/modules/checkTextInputs.js
@@ -1,6 +1,6 @@
 const checkTextInputs = (selector) => {
     const txtInputs = document.querySelectorAll(selector);
-
+    // user can only to write russian register
     txtInputs.forEach(input => {
         input.addEventListener('keypress', function(e) {
             if (e.key.match(/[^а-яё 0-9]/ig)) {
